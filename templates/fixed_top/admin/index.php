784e35d9277a95cfe1c506aea50103959fd8a3c2
28hariskumar <28harish.kumar@gmail.com>
2015-07-10 19:32:54 +0530

    version 1 finish

--- templates/fixed_top/admin/index.php
+++ templates/fixed_top/admin/index.php
@@ -6,7 +6,7 @@
 	<ul>
 		<li><a href="<?php echo  $this->to_url('theme-settings');?>">Manage Theme</a></li>
 		<li><a href="<?php echo  $this->to_url('admin-user-list');?>">Manage Users</a></li>
-		<li><a href="<?php echo  $this->to_url('menu-list');?>">Manage Menus - (code still in progress)</a></li>
-		<li><a href="<?php echo  $this->to_url('widget-settings');?>">Manage Widgets - (code still in progress)</a></li>
+		<li><a href="#">Manage Menus - (will be added in next version)</a></li>
+		<li><a href="#">Manage Widgets - (will be added in next version)</a></li>
 	</ul>
 </div>
