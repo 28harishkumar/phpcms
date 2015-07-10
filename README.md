152b8f96ad0389144823fb524a5c7758f8a41cda
28hariskumar <28harish.kumar@gmail.com>
2015-07-10 21:54:47 +0530

    README updated and sql added

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ A truly free and open source self created cms demo by creating a todo app which 
 * Sleek, stylish, portable and well-distributed interface
 * Login, Log out and Register Users
 * A default to do application which add, edit and delete to do entries under different labels. Save a due date and calculate remaining time and mansion the progress of the work
-* Portability for modules and widgets just like wordpress
-* customizable themes ( new themes can be added just like wordpress)
+* Portability for modules and widgets
+* customizable themes ( new themes can be added)
+* Admin section
 * Privacy and Security
 * No Ads
 * No Spam
@@ -15,16 +16,20 @@ Contact [Harish](https://www.facebook.com/profile.php?id=100003606204068) for mo
 Installation
 -------
 
-The frontend is developed [Bootstrap](http://getbootstrap.com/). If you wish to test it out, install xampp and fork the repository to `~\xampp\htdocs`. Start apache and mysql services and go to `localhost\todo` .
+The frontend is developed [Bootstrap](http://getbootstrap.com/). If you wish to test it out, install xampp and fork the repository to `~\xampp\htdocs`. 
+
+* Change config.example.php to config.php and set required config as mention in file.
+* Import sql file
+* Start apache and mysql services and go to `localhost\phpcms` .
 
 tutorials
 ------------
 
-I am creating this cms as well as tutorial for this cms. The tutorials are on my blogging webiste [Find All Together](http://www.findalltogether.com/wp/tag/create-cms/) . [Learn how to built a cms step by step](http://www.findalltogether.com/wp/tag/create-cms/).
+I am creating this cms as well as tutorial for this cms. The tutorials will be uploaded on my blogging webiste [Find All Together](http://www.findalltogether.com/wp/tag/create-cms/) . Old version is here [Learn how to built a cms step by step](http://www.findalltogether.com/wp/tag/create-cms/). New version is coming soon.
 
 Demo
 ------------
-The simple demo of this project is live on [http://todo.net46.net/](http://todo.net46.net/). 
+The simple demo of this project is live on [http://todo.findalltogether.com](http://todo.findalltogether.com). NOTE: It will be activated on 12th July 2015.
 
 Contributing
 ------------
---
+++ cms_todo.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 10, 2015 at 07:42 PM
+-- Server version: 5.5.43-0+deb7u1
+-- PHP Version: 5.6.7-1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cms_todo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `option`
+--
+
+CREATE TABLE IF NOT EXISTS `option` (
+`id` int(11) NOT NULL,
+  `option_name` varchar(250) NOT NULL,
+  `option_value` varchar(250) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `option`
+--
+
+INSERT INTO `option` (`id`, `option_name`, `option_value`) VALUES
+(1, 'theme', 'fixed_top');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todolist`
+--
+
+CREATE TABLE IF NOT EXISTS `todolist` (
+`id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `desc` text NOT NULL,
+  `progress` smallint(3) NOT NULL DEFAULT '0',
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `user_id` bigint(20) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` bigint(20) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `confirm_code` varchar(256) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '0',
+  `role` enum('admin','demo_checker','user') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'demo_checker',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `option`
+--
+ALTER TABLE `option`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `todolist`
+--
+ALTER TABLE `todolist`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `option`
+--
+ALTER TABLE `option`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `todolist`
+--
+ALTER TABLE `todolist`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=43;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
